6a6a32410fb885d0ebe3590b100a9578d0988155
Tony Martin <tony.martin@astehelsinki.fi>
2016-07-31 22:19:59 +0300

    DynamicArray testing

---
+++ test1/game/Tile.h
@@ -0,0 +1,13 @@
+#ifndef __TILE_H__
+#define __TILE_H__
+
+class Tile {
+
+public:
+
+  char character = '.';
+
+  Tile( char character ) : character( character ) {}
+};
+
+#endif
--- test1/game/main.cpp
+++ test1/game/main.cpp
@@ -1,42 +1,13 @@
 #include <iostream>
+#include <vector>
 #include "ncurses.h"
 
-struct Tile {
-  char character = '.';
-};
-
-class World {
-
-public:
-
-  static const int WORLD_WIDTH = 32;
-  static const int WORLD_HEIGHT = 16;
-  static const int WORLD_SIZE = ( WORLD_WIDTH * WORLD_HEIGHT );
-
-  Tile tiles[ WORLD_WIDTH * WORLD_HEIGHT ] = {};
-
-  void print() {
-
-    for ( size_t i = 0; i < WORLD_SIZE; ++i ) {
-      std::cout << this->tiles[ i ].character;
-      if ( i % WORLD_WIDTH == WORLD_WIDTH - 1 )
-        std::cout << std::endl;
-    }
-  }
-};
+#include "World.h"
+#include "Tile.h"
 
 int main( int argc, char** argv ) {
 
-  World world;
-
-  for ( int i = 1; i < argc && i < World::WORLD_HEIGHT; ++i ) {
-    for ( size_t j = 0; argv[ i ][ j ] != 0 && j < World::WORLD_WIDTH; ++j ) {
-      Tile tile;
-      tile.character = argv[ i ][ j ];
-      world.tiles[ i * World::WORLD_WIDTH + j ] = tile;
-    }
-  }
-
+  World world( std::vector<char*>( argv + 1, argv + argc ) );
   world.print();
 
   return 0;
